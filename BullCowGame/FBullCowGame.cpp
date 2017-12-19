dd5f9f7183e688d9e0ca296df9055da3c4f45112
reirby <andreirby@gmail.com>
2017-12-19 14:13:16 +0300

    Update FBullCowGame.cpp

--- BullCowGame/FBullCowGame.cpp
+++ BullCowGame/FBullCowGame.cpp
@@ -11,6 +11,7 @@ FBullCowGame::FBullCowGame() {	Reset(); } // default constructor
 
 int32 FBullCowGame::GetCurrentTry() const { return MycurrentTry; }
 int32 FBullCowGame::GetHiddenWordLength() const {return MyHiddenWord.length();}
+FString FBullCowGame::GetHiddenWord() { return MyHiddenWord; }
 bool FBullCowGame::IsGameWon() const { return bGameIsWon;}
 
 int32 FBullCowGame::GetMaxTries() const { 
@@ -20,8 +21,7 @@ int32 FBullCowGame::GetMaxTries() const {
 
 void FBullCowGame::Reset()
 {
-	const FString HIDDEN_WORD = "slone"; //this must be an isogram
-
+	const FString HIDDEN_WORD = MyHiddenWord;  //this is set in SetHiddenWord
 	MyHiddenWord = HIDDEN_WORD;
 	MycurrentTry = 1;
 	bGameIsWon = false;
@@ -110,3 +110,31 @@ bool FBullCowGame::IsLowercase(FString Word) const {
 	}
 	return true;
 }
+
+int32 FBullCowGame::SetHiddenWord(int32 WordLength)
+{
+	int32 random7 = rand() % 7;
+	std::string SixLetterWords[7] = {"margin", "singer", "mosaic", "egoism","mirage","sailor","mohair"};
+	std::string FiveLetterWords[7] = {"slone", "singer", "plane", "moral","storm","magot","ratio"};
+	std::string FourLetterWords[7] = {"corn", "coma", "soap", "sumo","bear","worm","risk"};
+	std::string ThreeLetterWords[7] = {"air", "jam", "art", "pig","sea","arm","ham"};
+
+	switch (WordLength) {
+	case 3:
+		MyHiddenWord = ThreeLetterWords[random7];
+		break;
+	case 4:
+		MyHiddenWord = FourLetterWords[random7];
+		break;
+	case 5:
+		MyHiddenWord = FiveLetterWords[random7];
+		break;
+	case 6:
+		MyHiddenWord = SixLetterWords[random7];
+		break;
+	default:
+		MyHiddenWord = "slone";
+	}
+	return 0;
+}
+
